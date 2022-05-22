813b42bdc37051384d35a21cefd6267f8b57772a
viekesh <vikeshgaikwad1@gmail.com>
2022-05-22 13:50:29 +0530

    Swipe Buttons Completes

--- frontend/src/Projects/TinderClone/Components/SwipeButton/SwipeButton.jsx
+++ frontend/src/Projects/TinderClone/Components/SwipeButton/SwipeButton.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import ReplayIcon from '@mui/icons-material/Replay';
+import CloseIcon from '@mui/icons-material/Close';
+import StarRateIcon from '@mui/icons-material/StarRate';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FlashOnIcon from '@mui/icons-material/FlashOn';
+import AppsIcon from '@mui/icons-material/Apps';
+import { IconButton } from '@mui/material';
+import './SwipeButton.css';
 
 const SwipeButton = () => {
     return (
-        <div className='swipe-buttons'>
+        <div className='swipe-buttons y-axis-center'>
+            <IconButton className="swipe-buttons_repeat">
+                <ReplayIcon />
+            </IconButton>
+            <IconButton className="swipe-buttons_left">
+                <CloseIcon />
+            </IconButton>
+            <IconButton className="swipe-buttons_star">
+                <StarRateIcon />
+            </IconButton>
+            <IconButton className="swipe-buttons_right">
+                <FavoriteIcon />
+            </IconButton>
+            <IconButton className="swipe-buttons_lightning">
+                <FlashOnIcon />
+            </IconButton>
         </div>
     )
 }
